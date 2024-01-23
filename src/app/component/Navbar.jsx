56932e9724b9cbde83927b21202444ed889d8e08
pushpendrapatel8055 <pushpendrapatel8055@gmail.com>
2024-01-23 10:43:22 +0530

    topbar menu in mobile fix

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -33,8 +33,8 @@ function onSound(){
     {isOn ? <Image className="cursor-pointer" onClick={offSound} width={30} height={30} src="/sound.png" />: <Image  className="cursor-pointer" onClick={onSound} width={30} height={30} src="/mute.png" />}
 
 
-        <a target="blank" href="https://nextjs-portfolio-five-lilac.vercel.app/1year_exp_in react&nodejs.pdf" className="bg-gradient-to-r from-red-500 to-teal-500 hover:from-pink-500 hover:to-yellow-500 px-2 py-1 mx-2 rounded-sm  font-thin  font-mono ">Resume </a>
-        <a  href="whatsapp://send?text=Hello pushpendra !&phone=+918516064332" className=" flex items-center gap-1 px-2 py-1 rounded-sm font-mono font-light bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"> <Image width={20} height={20} src="/whatsapp.png"  />  contact Me</a>
+        <a target="blank" href="https://nextjs-portfolio-five-lilac.vercel.app/1year_exp_in react&nodejs.pdf" className="bg-gradient-to-r from-red-500 text-sm to-teal-500 hover:from-pink-500 hover:to-yellow-500 px-2 py-1 mx-2 rounded-sm  font-thin  font-mono ">Resume </a>
+        <a  href="whatsapp://send?text=Hello pushpendra !&phone=+918516064332" className=" flex items-center gap-1 px-2 py-1 rounded-sm font-mono font-light bg-gradient-to-r text-sm from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"> <Image width={20} height={20} src="/whatsapp.png"  />  contact Me</a>
 {/* <div className="flex mx-1 justify-between items-center">
   <Image width={30} height={30} src="/gmail.png"  />
   <p className="text-xs">pushpendrapatel8055@gmail.com</p>
